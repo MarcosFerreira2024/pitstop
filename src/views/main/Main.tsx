ad9526e867d304276640757ea0d393e83014c291
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2025-02-20 05:40:58 -0300

    Update Main.tsx

--- src/views/main/Main.tsx
+++ src/views/main/Main.tsx
@@ -1,10 +1,15 @@
 import Header from '@/components/Header/Header'
+import CarList from '@/components/Main/CarList'
+import { carData } from '@/data/testeCars'
 import React from 'react'
 
 function Main() {
   return (
-    <div className='max-w-[1440px] mx-auto  '>
+    <div className='max-w-[1440px] mx-auto   '>
       <Header/>
+      <div className='flex  px-5 flex-wrap max-w-[1440px]  mt-[60px]  mb-5 justify-between gap-5 '>
+      <CarList carro={carData}/>
+      </div>
     </div>
   )
 }
