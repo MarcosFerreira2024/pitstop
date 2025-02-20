7d0ddfb8bd33cd05c88dd6dd430c3fac0bfd023e
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2025-02-20 04:25:59 -0300

    fonts roboto e poppins

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,21 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+import { Poppins } from "next/font/google";
+
+import { Roboto } from "next/font/google"
+
+const roboto = Roboto({
+  weight: "400",
   subsets: ["latin"],
+
+  variable: "--font-roboto",
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const poppins = Poppins({
+  weight: ["400"],
   subsets: ["latin"],
+  variable: "--font-poppins",
 });
 
 export const metadata: Metadata = {
@@ -25,7 +31,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${roboto.variable} ${poppins.variable} bg-background antialiased overflow-hidden `}
       >
         {children}
       </body>
