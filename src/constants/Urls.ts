aca2d121520095161141b34df9f14a4568efa3ae
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2025-02-22 03:53:51 -0300

    Update Urls.ts

--- src/constants/Urls.ts
+++ src/constants/Urls.ts
@@ -1,11 +1,14 @@
 
 const production = true
 
-const baseURL = production? "http://localhost:3000" : ""
+const baseURL = production? "http://localhost:3000/api" : ""
 
 
 
 export const URLS = {
     login: `${baseURL}/auth/login`,
-    register: `${baseURL}/auth/register`
+    register: `${baseURL}/auth/register`,
+    car: {
+        setId: (id:string) => `${baseURL}/car/${id}`
+    }
 }
